dd35d73eebcfde131e2fe26ef884e6d1cf3bb9dc
BeatriceV99 <s4801601@studenti.unige.it>
2024-07-15 23:58:00 +0200

    aggiunta powerpoint

--- website/json/script.js
+++ website/json/script.js
@@ -1,3 +1,23 @@
+// funzione per apertura popup del login e della registrazione
+function openLoginPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("loginPopup").style.display = "none";
+}
+
+function showRegisterForm() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "block";
+}
+
+function showLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("registerForm").style.display = "none";
+}
+
+// funzione per apertura popup degli eventi
 function showPopup(id) {
     document.getElementById(id).style.display = 'flex';
 }
